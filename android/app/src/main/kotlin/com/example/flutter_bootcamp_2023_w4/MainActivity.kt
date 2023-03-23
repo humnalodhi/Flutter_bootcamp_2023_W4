16e2f75042cd2439fb5e923b809ce48669b67f6d
humnalodhi <humna02468@gmail.com>
2023-03-23 17:28:04 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_bootcamp_2023_w4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bootcamp_2023_w4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+final RiverpodProvider = Provider((ref) => 'Flutter State Management: Riverpod Provider');
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter bootcamp 2023 - W4',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter bootcamp 2023 - W4'),
+      ),
+      body: Consumer(
+        builder: (_, WidgetRef ref, __){
+          final riverpod_provider = ref.watch(RiverpodProvider);
+          return Center(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(riverpod_provider, style: TextStyle(fontSize: 18),),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+
+    );
+  }
+}
